b08865fc1819cc9de1060c8e58abc0cf011c02f0
drog41813 <daniel.lee042003@gmail.com>
2023-07-10 17:26:37 -0500

    rearranged comments in JS and removed html comments

--- script.js
+++ script.js
@@ -2,33 +2,12 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(document).ready(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
 
-  //$(".saveButton").on("click", function() {
-    //console.log('this');
-    //var timeBlock = $(this).child("description").val();
-    //var time = $(this).parent().attr('id');
-
-    //localStorage.setItem(time, timeBlock);
-  //})
+// TODO: Add code to display the current date in the header of the page.
 var current = dayjs().format('dddd, MMMM D');
 $('#currentDay').text(current);
 
-//$("#hour-9 .description").val(localStorage.getItem("hour-9"));
-//$("#hour-10 .description").val(localStorage.getItem("hour-10"));
-//$("#hour-11 .description").val(localStorage.getItem("hour-11"));
-//$("#hour-12 .description").val(localStorage.getItem("hour-12"));
-//$("#hour-13 .description").val(localStorage.getItem("hour-13"));
-//$("#hour-14 .description").val(localStorage.getItem("hour-14"));
-//$("#hour-15 .description").val(localStorage.getItem("hour-15"));
-//$("#hour-16 .description").val(localStorage.getItem("hour-16"));
-//$("#hour-17 .description").val(localStorage.getItem("hour-17"));
-
+var currentHour = dayjs().format('HH');
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -36,40 +15,33 @@ $('#currentDay').text(current);
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   //
-var currentHour = dayjs().format('HH');
-
 $(".time-block").each(function(){
   var blockHour = $(this).attr("id").split("-")[1];
 
+  // TODO: Add code to get any user input that was saved in localStorage and set
+  // the values of the corresponding textarea elements. HINT: How can the id
+  // attribute of each time-block be used to do this?
+  //
   var textEntry = localStorage.getItem(blockHour);
   var textArea = $(this).find(".description");
   textArea.val(textEntry);
 
   if (blockHour < currentHour) {
     $(this).find(".description").addClass("past");
-    //$(this).addClass("past")
-    //$(this).removeClass("future")
-    //$(this).removeClass("present")
   } else if (blockHour === currentHour) {
     $(this).find(".description").addClass("present");
-    //$(this).addClass("present")
-    //$(this).removeClass("past")
-   //$(this).removeClass("future")
   } else {
     $(this).find(".description").addClass("future");
-    //$(this).addClass("future")
-    //$(this).removeClass("past")
-    //$(this).removeClass("present")
   }
 });
  
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
   
-  // TODO: Add code to display the current date in the header of the page.
-
+  // TODO: Add a listener for click events on the save button. This code should
+  // use the id in the containing time-block as a key to save the user input in
+  // local storage. HINT: What does `this` reference in the click listener
+  // function? How can DOM traversal be used to get the "hour-x" id of the
+  // time-block containing the button that was clicked? How might the id be
+  // useful when saving the description in local storage?
   $(".saveBtn").on("click", function(){
     var key = $(this).parent().attr("id").split("-")[1];
     var value = $(this).parent().find(".description").val();
